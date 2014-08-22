fdfd5a4caa74cb210f4fac13e43b91010ca69501
Flemming Jønsson <frj@trifork.com>
2014-08-22 16:40:15 +0200

    Added medicinecard20140801 namespace prefix and namespace http://www.dkma.dk/medicinecard/xml.schema/2014/08/01 to FmkNamespaces class so that NamespaceOracleGenerator does not fail

--- src/dk/medicinkortet/xmlschema/FmkNamespaces.java
+++ src/dk/medicinkortet/xmlschema/FmkNamespaces.java
@@ -17,6 +17,7 @@ public class FmkNamespaces {
 		namespaces.addNamespace("medicinecard2013pem", "http://www.dkma.dk/medicinecard/xml.schema/2013/pem");
 		namespaces.addNamespace("medicinecard20140501", "http://www.dkma.dk/medicinecard/xml.schema/2014/05/01");
 		namespaces.addNamespace("medicinecard20140601", "http://www.dkma.dk/medicinecard/xml.schema/2014/06/01");
+		namespaces.addNamespace("medicinecard20140801", "http://www.dkma.dk/medicinecard/xml.schema/2014/08/01");
 		namespaces.addNamespace("ssi2013", "http://www.ssi.dk/nsi/xml.schema/2013/01/01");
 		namespaces.addNamespace("cpr", "http://rep.oio.dk/cpr.dk/xml/schemas/core/2005/03/18/");
 		namespaces.addNamespace("cpr2002", "http://rep.oio.dk/cpr.dk/xml/schemas/core/2002/06/28/");
