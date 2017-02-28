d189c4886c9b13ff8e806cbd8ff9524aadf07902
Michael Berg <mbe@trifork.com>
2017-02-28 09:47:11 +0100

    Manglende definition af 1.4.4.E2 namespace

--- src/dk/medicinkortet/xmlschema/FmkNamespaces.java
+++ src/dk/medicinkortet/xmlschema/FmkNamespaces.java
@@ -20,6 +20,7 @@ public class FmkNamespaces {
 		namespaces.addNamespace("medicinecard20140801", "http://www.dkma.dk/medicinecard/xml.schema/2014/08/01");
 		namespaces.addNamespace("medicinecard20150101", "http://www.dkma.dk/medicinecard/xml.schema/2015/01/01");
 		namespaces.addNamespace("medicinecard20150101E1", "http://www.dkma.dk/medicinecard/xml.schema/2015/01/01/E1");
+		namespaces.addNamespace("medicinecard20150101E2", "http://www.dkma.dk/medicinecard/xml.schema/2015/01/01/E2");
         namespaces.addNamespace("medicinecard20150601", "http://www.dkma.dk/medicinecard/xml.schema/2015/06/01");
         namespaces.addNamespace("medicinecard20150601E2", "http://www.dkma.dk/medicinecard/xml.schema/2015/06/01/E2");
 		namespaces.addNamespace("ssi2013", "http://www.ssi.dk/nsi/xml.schema/2013/01/01");
