e5a892542a749d04469fd9595027d44b9c13f8d1
Markus Andreassen <maa@trifork.com>
2018-10-03 13:52:10 +0200

    FMK-4720 Oprettet xsd og wsdl for 1.4.4E4

--- src/dk/medicinkortet/xmlschema/FmkNamespaces.java
+++ src/dk/medicinkortet/xmlschema/FmkNamespaces.java
@@ -16,6 +16,7 @@ public class FmkNamespaces {
 		namespaces.addNamespace("medicinecard20150101", "http://www.dkma.dk/medicinecard/xml.schema/2015/01/01");
 		namespaces.addNamespace("medicinecard20150101E1", "http://www.dkma.dk/medicinecard/xml.schema/2015/01/01/E1");
 		namespaces.addNamespace("medicinecard20150101E2", "http://www.dkma.dk/medicinecard/xml.schema/2015/01/01/E2");
+		namespaces.addNamespace("medicinecard20150101E4", "http://www.dkma.dk/medicinecard/xml.schema/2015/01/01/E4");
         namespaces.addNamespace("medicinecard20150601", "http://www.dkma.dk/medicinecard/xml.schema/2015/06/01");
         namespaces.addNamespace("medicinecard20150601E2", "http://www.dkma.dk/medicinecard/xml.schema/2015/06/01/E2");
         namespaces.addNamespace("medicinecard20150601E3", "http://www.dkma.dk/medicinecard/xml.schema/2015/06/01/E3");
