bd411d543cb080eadec2a42062f43ee66363aa86
Flemming Jørgensen <fjo@trifork.com>
2025-10-14 19:18:07 +0200

    JTE test

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -4,4 +4,7 @@ libraries{
         java_version = 11
     }
     sonarqube_maven
+    archive {
+        artifacts = 'fmk-schemas/mc160_wsdl/target/*.zip, fmk-schemas/mc160idws_wsdl/target/*.zip, fmk-schemas/mc160_wsdl/target/resources/wsdl/*.wsdl, fmk-schemas/mc160idws_wsdl/target/resources/wsdl/*.wsdl'
+    }
 }
