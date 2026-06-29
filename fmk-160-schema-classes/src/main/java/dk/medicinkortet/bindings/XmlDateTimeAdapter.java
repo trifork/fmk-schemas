933ab4797e8d32154f497b9de096c404a6b20623
Adam Yasser Tallouzi <adta@trifork.com>
2026-06-29 11:32:10 +0200

    FMK-13403: tidszone håndtering (#224)

--- fmk-160-schema-classes/src/main/java/dk/medicinkortet/bindings/XmlDateTimeAdapter.java
+++ fmk-160-schema-classes/src/main/java/dk/medicinkortet/bindings/XmlDateTimeAdapter.java
@@ -2,36 +2,37 @@ package dk.medicinkortet.bindings;
 
 import jakarta.xml.bind.annotation.adapters.XmlAdapter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class XmlDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
-    private static final DateTimeFormatter ISO_LOCAL_DATE_TIME = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
     private static final DateTimeFormatter ISO_OFFSET_DATE_TIME = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
-    private static final DateTimeFormatter ISO_LOCAL_DATE = DateTimeFormatter.ISO_LOCAL_DATE;
+    private static final ZoneId COPENHAGEN_ZONE_ID = ZoneId.of("Europe/Copenhagen");
 
     @Override
-    public LocalDateTime unmarshal(String value) {
+    public LocalDateTime unmarshal(String value) throws Exception {
         if (value == null || value.isEmpty()) {
             return null;
         }
-        try {
-            return LocalDateTime.parse(value, ISO_LOCAL_DATE_TIME);
-        } catch (DateTimeParseException ignore) {
-            return OffsetDateTime.parse(value, ISO_OFFSET_DATE_TIME).toLocalDateTime();
-        }
+
+        return OffsetDateTime.parse(value, ISO_OFFSET_DATE_TIME)
+                .atZoneSameInstant(COPENHAGEN_ZONE_ID)
+                .toLocalDateTime();
     }
 
     @Override
-    public String marshal(LocalDateTime value) {
+    public String marshal(LocalDateTime value) throws Exception {
         if (value == null) {
             return null;
         }
-        return value.atOffset(ZoneOffset.UTC).format(ISO_OFFSET_DATE_TIME);
+
+        return value.atZone(COPENHAGEN_ZONE_ID)
+                .withZoneSameInstant(ZoneOffset.UTC)
+                .toOffsetDateTime()
+                .format(ISO_OFFSET_DATE_TIME);
     }
 
 }
