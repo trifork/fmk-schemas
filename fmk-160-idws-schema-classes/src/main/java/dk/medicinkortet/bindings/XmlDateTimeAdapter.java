2bcdb510ff26e4ec8600c06e427dca6e6eacb7a6
Adam Yasser Tallouzi <adta@trifork.com>
2026-06-29 09:35:33 +0200

    offset date time

--- fmk-160-idws-schema-classes/src/main/java/dk/medicinkortet/bindings/XmlDateTimeAdapter.java
+++ fmk-160-idws-schema-classes/src/main/java/dk/medicinkortet/bindings/XmlDateTimeAdapter.java
@@ -2,23 +2,23 @@ package dk.medicinkortet.bindings;
 
 import jakarta.xml.bind.annotation.adapters.XmlAdapter;
 
-import java.time.ZonedDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class XmlDateTimeAdapter extends XmlAdapter<String, ZonedDateTime> {
-    private static final DateTimeFormatter ISO_ZONED_DATE_TIME = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+public class XmlDateTimeAdapter extends XmlAdapter<String, OffsetDateTime> {
+    private static final DateTimeFormatter ISO_OFFSET_DATE_TIME = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
     @Override
-    public ZonedDateTime unmarshal(String value) throws Exception {
+    public OffsetDateTime unmarshal(String value) throws Exception {
         if (value == null || value.isEmpty()) {
             return null;
         }
 
-        return ZonedDateTime.parse(value, ISO_ZONED_DATE_TIME);
+        return OffsetDateTime.parse(value, ISO_OFFSET_DATE_TIME);
     }
 
     @Override
-    public String marshal(ZonedDateTime value) throws Exception {
+    public String marshal(OffsetDateTime value) throws Exception {
         return value.toString();
     }
 
