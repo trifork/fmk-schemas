3a98f1b464f722b8aa1511557f4a972da299bf60
Louise Bonderup Dohn <lbdo@trifork.com>
2026-08-11 15:20:16 +0200

    bumped dependencies

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -7,4 +7,9 @@ libraries{
     archive {
         artifacts = 'mc160_wsdl/target/*.zip, mc160idws_wsdl/target/*.zip, mc160_wsdl/target/resources/wsdl/*.wsdl, mc160idws_wsdl/target/resources/wsdl/*.wsdl'
     }
+    verify {
+        excluded_list = [
+                'CVE-2021-40690', 'CVE-2013-4517', 'CVE-2023-44483', // org.apache.santuario:xmlsec venter på seal
+        ]
+    }
 }
